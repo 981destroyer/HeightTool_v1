6f1c016717cd38d7d290ed31fc49ba590a9d2bc0
981destroyer <109875742+981destroyer@users.noreply.github.com>
2022-07-24 01:42:45 +0200

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class main {
+    static double maleChild;
+    static double femaleChild;
+    double dadHeight;
+    double momHeight;
+    static Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args) {
+        //how tall is the dad?
+        System.out.println("How tall is your dad? (in cm)");
+        double dadHeight = userInput.nextInt();
+
+        //User Mom height:
+        System.out.println("How tall is your mom? (in cm)");
+        double momHeight = userInput.nextInt();
+
+        //predicted height:
+        femaleChild = dadHeight+momHeight-13;
+        maleChild = dadHeight+momHeight+13;
+
+        System.out.println("According to the basic method:");
+        System.out.println("Your daughter will be " + femaleChild/2 + "cm tall");
+        System.out.println("Your son will be " + maleChild/2 + "cm tall");
+    }
+}
